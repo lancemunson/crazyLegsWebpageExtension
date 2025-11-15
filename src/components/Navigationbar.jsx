b5308a994a607208a130bae2ef0e0db747e612c0
Owner <lmunson@wisc.edu>
2025-11-15 13:06:53 -0600

    added style to navbar

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -1,29 +1,33 @@
-import { Nav, Button, Navbar } from "react-bootstrap";
+import { Nav, Button, Navbar, Image } from "react-bootstrap";
 import { HashRouter, Routes, Route, NavLink} from "react-router";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import Registration from "./pages/registration";
 import Course from "./pages/course";
 
+import CrazyLegsLogo from "./../assets/crazyLegsLogo.png";
+
 export default function Navigationbar(props){
-    return  <Navbar fixed="top">
+    return  <Navbar>
                 <HashRouter>
                     <Nav id="navBar" >
                         <Button className="navButton">
-                            <NavLink to="/">Home</NavLink>
+                            <NavLink className="link" to="/">Home</NavLink>
                         </Button>
                         <Button className="navButton">
-                            <NavLink to="/login">Login</NavLink>
+                            <NavLink className="link" to="/registration">Registration</NavLink>
                         </Button>
                         <Button className="navButton">
-                            <NavLink to="/course">Course</NavLink>
+                            <NavLink className="link" to="/login">Login</NavLink>
                         </Button>
                         <Button className="navButton">
-                            <NavLink to="/registration">Registration</NavLink>
+                            <NavLink className="link" to="/course">Course</NavLink>
                         </Button>
+                
+                        <Image src={CrazyLegsLogo}height="80rem" width="170rem" alt="Crazy Legs Classic Logo"/>
                     </Nav>
                     <Routes>
-                        <Route path="/" element={<Homepage/>}/>
+                        <Route path="/" element={<Homepage className="page"/>}/>
                         <Route path="/login" element={<LoginPage/>}/>
                         <Route path="/course" element={<Course/>}/>
                         <Route path="/registration" element={<Registration/>}/>
